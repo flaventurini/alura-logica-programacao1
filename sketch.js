51573e3263aec68fe6c68dc70d3273e8bb2ff7ea
Flávia Venturini <100423453+flaventurini@users.noreply.github.com>
2022-11-26 17:46:37 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,69 @@
+//Arquivo JS usado no p5.js
+
+//Variáveis da Bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametroBolinha = 15;
+let raioBolinha = diametroBolinha / 2;
+
+//Velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//Variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaRaquete = 90;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  movimentaMinhaRaquete();
+  verificaColisaoRaquete();
+}
+
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametroBolinha);
+}
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda() {
+  if (xBolinha + raioBolinha > width || xBolinha - raioBolinha <0) {
+    velocidadeXBolinha *= -1;
+  }
+  
+  if (yBolinha + raioBolinha  > height || yBolinha - raioBolinha < 0) {
+    velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete() {
+  rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+}
+
+function movimentaMinhaRaquete() {
+  if (keyIsDown(UP_ARROW)) {
+    yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete() {
+  if (xBolinha - raioBolinha < xRaquete + larguraRaquete && yBolinha - raioBolinha < yRaquete + alturaRaquete && yBolinha + raioBolinha > yRaquete) {
+    velocidadeXBolinha *= -1;
+  }
+}
